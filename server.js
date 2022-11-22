3ace17b21fd4aeb2cd85cedd13ca6319f11013a8
Mnishek <shavyrina.marina@mail.ru>
2022-11-22 21:21:51 +0100

    no adminSDK

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use('/api/admin', require('./routes/admin.routes'))
 app.use('/api/booksearch', require('./routes/google.book.routes'))
 
 if (process.env.NODE_ENV === 'production') {
